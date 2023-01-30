815ced95e1e58b9d78b506adfbb1048ee26f73cd
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2023-01-31 00:04:48 +0530

    Committing Latest

--- app/src/main/java/com/example/unsplash/data/local/dao/UnsplashImageDao.kt
+++ app/src/main/java/com/example/unsplash/data/local/dao/UnsplashImageDao.kt
@@ -12,7 +12,7 @@ interface UnsplashImageDao {
     @Query("SELECT * FROM unsplash_image_table")
     fun getAllImages(): PagingSource<Int, UnsplashImage>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict =  OnConflictStrategy.REPLACE)
     suspend fun addImages(images: List<UnsplashImage>)
 
     @Query("DELETE FROM unsplash_image_table")
