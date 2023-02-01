aa83249cbe26daa860281fa9fc56217d0fefbf3f
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2023-02-01 23:48:58 +0530

    Committing Latest

---
+++ app/src/main/java/com/example/unsplash/Screens/home/HomeTopBar.kt
@@ -0,0 +1,36 @@
+package com.example.unsplash.Screens.home
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeTopBar(
+    onSearchClicked: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Home",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
